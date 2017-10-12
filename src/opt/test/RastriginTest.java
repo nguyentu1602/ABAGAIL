ec2aedcbb7358f6f6345b39afc7ae38ce4ed4fdc
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2017-10-13 00:06:46 +0100

    test for rastrigin

--- src/opt/test/RastriginTest.java
+++ src/opt/test/RastriginTest.java
@@ -3,7 +3,6 @@ package opt.test;
 import opt.EvaluationFunction;
 import opt.example.RastriginEvaluationFunction;
 import shared.Instance;
-import util.linalg.DenseVector;
 
 import java.util.Arrays;
 
@@ -23,14 +22,39 @@ public class RastriginTest {
 
         EvaluationFunction ef = new RastriginEvaluationFunction();
 
-        double[] optim = new double[N];
-        Arrays.fill(optim, 0);
+        double[] optim_arr = new double[N];
+        Arrays.fill(optim_arr, 0);
 
-        Instance inst = Instance( optim );
+        // Instance optim = new Instance( optim_arr );
+       // System.out.println(ef.value(optim));
 
-        System.out.println(ef.value(rhc.getOptimal()));
+        // Test case: -1, -1
+        String firstStr  = Integer.toBinaryString(Float.floatToRawIntBits(-1.0f));
+        String secondStr = Integer.toBinaryString(Float.floatToRawIntBits(-1.0f));
+        String fullBitStr = firstStr + secondStr;
 
-        System.out.println(ef.value(mimic.getOptimal()));
+        assert fullBitStr.length() == N;
+
+        String[] bitStrArr = fullBitStr.split("");
+        for(int i = 0; i < bitStrArr.length; i++){
+            optim_arr[i] = Double.parseDouble(bitStrArr[i]);
+        }
+
+        System.out.println(ef.value(new Instance( optim_arr )));
+
+        // Test case: 5, 3  - need to patch the zero infront for the missing sign bit
+        String firstStr1  = "0" + Integer.toBinaryString(Float.floatToRawIntBits(5.0f));
+        String secondStr1 = "0" + Integer.toBinaryString(Float.floatToRawIntBits(3.0f));
+        String fullBitStr1 = firstStr1 + secondStr1;
+
+        assert fullBitStr1.length() == N;
+
+        String[] bitStrArr1 = fullBitStr1.split("");
+        for(int i = 0; i < bitStrArr1.length; i++){
+            optim_arr[i] = Double.parseDouble(bitStrArr1[i]);
+
+        }
+
+       System.out.println(ef.value(new Instance( optim_arr )));
     }
-
 }
